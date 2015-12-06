ffdeab7714a548ae5ab629a752b4be40a0e52269
Travis Bloomfield <mail@travisbloomfield.com>
2015-12-06 18:59:17 +0000

    Created validations and create action.

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -7,9 +7,11 @@ class ContactsController < ApplicationController
       @contact = Contact.new(contact_params)
       
       if @contact.save
-          redirect_to new_contact_path, notice: "Message sent."
+          flash[:success] = "Message sent."
+          redirect_to new_contact_path
       else
-          redirect_to new_contact_path, notice: "Error creating message."
+          flash[:danger] = "Error sending message, message was not sent."
+          redirect_to new_contact_path
       end
   end
   
