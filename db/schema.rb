3907495e604b3750b983eb8eb0be6343e8b3f9ab
Travis Bloomfield <mail@travisbloomfield.com>
2015-12-16 06:44:18 +0000

    Modified devise to incorporate stripe.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151210193321) do
+ActiveRecord::Schema.define(version: 20151216064051) do
 
   create_table "contacts", force: true do |t|
     t.string "name"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 20151210193321) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "plan_id"
+    t.string   "stripe_customer_token"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
