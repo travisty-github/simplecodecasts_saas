c5cc318afe56a072dc4252b7162cc28d935977d0
Travis Bloomfield <mail@travisbloomfield.com>
2015-12-21 20:03:14 +0000

    Created form partial and edit action.

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -17,6 +17,11 @@ class ProfilesController < ApplicationController
             render action: :new
         end
     end
+    
+    def edit
+        @user = User.find(params[:user_id])
+        @profile = @user.profile
+    end
 
     private
         def profile_params
