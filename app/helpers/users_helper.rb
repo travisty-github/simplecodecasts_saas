8b956e58a7796987624592cb69089ec3b46568e4
Travis Bloomfield <bloomfield.travis@gmail.com>
2015-12-24 09:58:58 +0000

    Created pro labels.

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,16 +1,26 @@
 module UsersHelper
-    
+
+    # Show the approrpiate icon based on job title
     def job_title_icon(user)
         case user.profile.job_title
             when "Developer"
-                "<i class='fa fa-code'></i>".html_safe 
+                "<i class='fa fa-code'></i>".html_safe
             when "Entrepreneur"
-                "<i class='fa fa-lightbulb-o'></i>".html_safe 
+                "<i class='fa fa-lightbulb-o'></i>".html_safe
             when "Investor"
-                "<i class='fa fa-usd'></i>".html_safe 
+                "<i class='fa fa-usd'></i>".html_safe
         else
             return
         end
     end
-    
+
+    # Show a label if the user has a pro account (plan_id 2)
+    def pro_label(user)
+      if user.plan_id == 2
+        return "<span class='label label-warning'>PRO</span>".html_safe
+      else
+        return
+      end
+    end
+
 end
